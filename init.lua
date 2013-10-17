4f5aeff4249d289e08695082bab436f0f42788fb
webdesigner97 <Christian_D_97@gmx.de>
2013-10-17 17:18:58 +0200

    Some kind of movement

--- init.lua
+++ init.lua
@@ -1,6 +1,16 @@
 --[[
 	StreetsMod: Experimental cars
 ]]
+local function get_single_accels(p)
+	local output = {x = 0, y = 0, z = 0}
+	local alpha = p.dir
+	local beta = 180 - 90 - alpha
+	local hyp = p.accel
+	output.x = math.sin(alpha) * hyp
+	output.z = math.sin(beta) * hyp
+	return output
+end
+
 minetest.register_entity(":streets:melcar",{
 	initial_properties = {
 		hp_max = 100,
@@ -17,6 +27,7 @@ minetest.register_entity(":streets:melcar",{
 		driver = nil,
 		on_ground = false,
 		max_speed = 10.0,
+		max_rpm = 4000,
 		accel = 4,
 		decel = 6,
 		gears = 3,
@@ -24,8 +35,9 @@ minetest.register_entity(":streets:melcar",{
 		
 		-- Runtime variables
 		speed = 0,
-		engine_rpm = 0,
+		rpm = 0,
 		gear = 0,
+		brake_factor = 1,
 		hud = {
 			gear,
 			rpm,
@@ -34,6 +46,8 @@ minetest.register_entity(":streets:melcar",{
 	on_activate = function(self)
 		-- Gravity
 		self.object:setacceleration({x=0,y= self.initial_properties.weight * 9.81 * -1,z=0})
+		self.props.rpm = 500
+		self.props.gear = 1
 	end,
 	on_rightclick = function(self,clicker)
 		if self.props.driver == nil then
@@ -78,6 +92,43 @@ minetest.register_entity(":streets:melcar",{
 		end
 	end,
 	on_step = function(self,dtime)
-		
+		-- Player controls
+		if self.props.driver then
+			local ctrl = minetest.get_player_by_name(self.props.driver):get_player_control()
+			-- up
+			if ctrl.up then
+				self.props.brake = 1
+				if self.props.rpm < self.props.max_rpm then
+					self.props.rpm = self.props.rpm + 40
+				end
+			else
+				if self.props.rpm >= 520 then
+					self.props.rpm = self.props.rpm - 20
+				end
+			end
+			-- down
+			if ctrl.down then
+				self.props.brake = -1
+				if self.props.rpm >= 520 then
+					self.props.rpm = self.props.rpm - 40
+				end
+			end
+			-- left
+			if ctrl.left then
+				self.object:setyaw(self.object:getyaw() + 1 * dtime)
+			end
+			-- right
+			if ctrl.right then
+				self.object:setyaw(self.object:getyaw() - 1 * dtime)
+			end
+		end
+		-- Calculate acceleration
+		if self.props.brake == 1 then
+			local accel = (self.props.rpm / 1000 - 0.5) * self.props.gear * self.props.brake_factor
+			self.object:setacceleration(get_single_accels({
+				dir = self.object:getyaw(),
+				accel = accel
+			}))
+		end
 	end
 })
