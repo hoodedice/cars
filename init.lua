85c58c6cfcf157f5234776f510c5ba406ae2899e
webdesigner97 <Christian_D_97@gmx.de>
2014-05-16 22:45:10 +0200

    base movement on fake acceleration. Builtin setacceleration causes odd behaviour

--- init.lua
+++ init.lua
@@ -17,9 +17,10 @@ minetest.register_entity(":streets:melcar",{
 	},
 	props = {
 		driver = nil,
-		max_speed = 10.0,
+		max_vel = 10.0,
 		accel = 6.25,
 		decel = 4.5,
+		brakeDecel = 9,
 		
 		-- Runtime variables
 		speed = 0,
@@ -28,13 +29,12 @@ minetest.register_entity(":streets:melcar",{
 		brake = false,
 		accelerate = false,
 		sound = nil,
+		vel = 0,
+		steer = false,
 		hud = {
 			gear,
 			rpm,
-		},
-		forces = {
-			{x = 0, y = -9.81, z = 0}
-		},
+		}
 	},
 	on_activate = function(self)
 		-- Gravity
@@ -82,21 +82,24 @@ minetest.register_entity(":streets:melcar",{
 			if ctrl.up then
 				self.props.brake = false
 				self.props.accelerate = true
-				if self.props.rpm < self.props.max_rpm then
-					self.props.rpm = self.props.rpm + 20
+				-- Only accelerate if max_speed not reached and player does not steer
+				if self.props.vel < self.props.max_vel and self.props.steer == false then
+					self.props.vel = self.props.vel + (self.props.accel * dtime)
 				end
 			else
 				self.props.accelerate = false
-				if self.props.rpm >= 520 then
-					self.props.rpm = self.props.rpm - 20
+				-- Decelerate down to more or less 0
+				if self.props.vel > 0 then
+					self.props.vel = self.props.vel - (self.props.decel * dtime)
 				end
 			end
 			-- down
 			if ctrl.down then
 				self.props.brake = true
 				self.props.accelerate = false
-				if self.props.rpm >= 520 then
-					self.props.rpm = self.props.rpm - 20
+				-- Brake down to more or less 0
+				if self.props.vel > 0 then
+					self.props.vel = self.props.vel - (self.props.brakeDecel * dtime)
 				end
 			else
 				self.props.brake = false
@@ -104,88 +107,30 @@ minetest.register_entity(":streets:melcar",{
 			-- left
 			if ctrl.left then
 				self.object:setyaw(self.object:getyaw() + 1 * dtime)
+				self.props.steer = true
+			else
+				self.props.steer = false
 			end
 			-- right
 			if ctrl.right then
 				self.object:setyaw(self.object:getyaw() - 1 * dtime)
+				self.props.steer = true
+			else
+				self.props.steer = false
 			end
-		end
-		-- Calculate acceleration
-		if self.props.brake == false then
-			accel = (self.props.rpm - 500) * self.props.accel
-			table.insert(self.props.forces, force2vec(self, {
-				dir = self.object:getyaw(),
-				accel = accel
-			}))
-		else
-			if merge_single_forces(self.object:getvelocity().x, self.object:getvelocity().z) > 0.1 then
-				table.insert(self.props.forces, force2vec(self,{
-					dir = self.object:getyaw(),
-					accel = -8000
-				}))
-			end
-		end
-		-- Slow down, if car doesn't accelerate
-		if self.props.accelerate == false and self.props.brake == false then
-			table.insert(self.props.forces, force2vec(self,{
-				dir = self.object:getyaw(),
-				accel = -8000 * self.props.decel
-			}))
-		end
-		-- Stop acceleration if max_speed reached
-		if merge_single_forces(self.object:getvelocity().x, self.object:getvelocity().z) >= self.props.max_speed and self.props.brake == false then
-			self.object:setacceleration({x = 0, y= -9.81, z = 0})
-			self.props.forces = {
-				{x = 0, y = -9.81, z = 0}
-			}
 		end
 		-- Stop if very slow (e.g. because driver brakes)
 		if math.abs(merge_single_forces(self.object:getvelocity().x, self.object:getvelocity().z)) < 0.1 and self.props.accelerate == false then
-			self.object:setacceleration({x=0,y= -9.81 ,z=0})
-			self.object:setvelocity({x=0,y=0,z=0})
-			self.props.forces = {
-				{x = 0, y = -9.81, z = 0}
-			}
-		end
-		--Calculate resulting acceleration		
-		local res = {x = 0, y = -9.81, z = 0}
-		local allX = {}
-		local allY = {}
-		local allZ = {}
-		
-		for k, v in pairs(self.props.forces) do
-			table.insert(allX, v.x)
-			table.insert(allY, v.y)
-			table.insert(allZ, v.z)
+			self.object:setvelocity({x = 0,y = self.object:getvelocity().y,z = 0})
+			return
 		end
 		
-		for k, v in pairs(allX) do
-			res.x = res.x + v
-		end
-		res.x = res.x / #allX
-		for k, v in pairs(allY) do
-			res.y = res.y + v
-		end
-		res.y = res.y / #allY
-		for k, v in pairs(allZ) do
-			res.z = res.z + v
-		end
-		res.z = res.z / #allZ
-		
-		-- Add centripetal force
-		local cf = (self.initial_properties.weight * merge_single_forces(self.object:getvelocity().x, self.object:getvelocity().z)) / (math.cos(math.deg(self.object:getyaw())) * merge_single_forces(res.x, res.z))
-		minetest.chat_send_all(cf)
-		
-		-- Acceleration = Force / Weight
-		res.x = res.x / self.initial_properties.weight
-		res.z = res.z / self.initial_properties.weight
-		
-		--Reset forces
-		self.props.forces = {
-			{x = 0, y = -9.81, z = 0}
-		}
-		
-		--Apply acceleration
-		self.object:setacceleration(res)
+		--Apply velocity
+		local finalVelocity = force2vec(self, {
+			dir = self.object:getyaw(),
+			force = self.props.vel
+		})
+		finalVelocity.y = self.object:getvelocity().y
+		self.object:setvelocity(finalVelocity)
 	end
 })
