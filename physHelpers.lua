18e7115eabecbc96c41329ea9a8f1d1780ea8212
webdesigner97 <Christian_D_97@gmx.de>
2014-05-17 14:54:49 +0200

    fix force2vec bug calculating with deg instead of rad

--- physHelpers.lua
+++ physHelpers.lua
@@ -1,8 +1,9 @@
 function force2vec(self,p)
 	local output = {x = 0, y = 0, z = 0}
 	local alpha = p.dir
-	local beta = 180 - 90 - alpha
+	local beta = math.pi - math.pi / 2 - alpha
 	local hyp = p.force
+	minetest.chat_send_all(alpha .. " -> " .. beta)
 	output.x = (math.sin(alpha) * hyp * -1)
 	output.z = (math.sin(beta) * hyp)
 	return output
